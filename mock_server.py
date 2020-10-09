e4064cfdff0746cce8b832501e4262107a9db8e9
hreinn91 <hreinnj@localhost.localdomain>
2020-10-09 18:26:53 +0200

    Simpler csv printer

--- mock_server.py
+++ mock_server.py
@@ -10,11 +10,24 @@ data = [
     ["2004/10/04","Randel Helms",'0879755725',4.50]
 ]
 
-@app.route('/generate', methods=['GET'])
-def generate():
+@app.route('/generate2', methods=['GET'])
+def generate2():
     output = excel.make_response_from_array(data, 'csv')
     output.headers["Content-type"] = "text/csv"
     return output
+
+
+@app.route('/generate')
+def generate():
+    csv = """"REVIEW_DATE","AUTHOR","ISBN","DISCOUNTED_PRICE"
+"1985/01/21","Douglas Adams",0345391802,5.95
+"1990/01/12","Douglas Hofstadter",0465026567,9.95
+"1998/07/15","Timothy ""The Parser"" Campbell",0968411304,18.99
+"1999/12/03","Richard Friedman",0060630353,5.95
+"2004/10/04","Randel Helms",0879755725,4.50"""
+    response = make_response(csv)
+    response.headers["Content-Disposition"] = "attachment; filename=books.csv"
+    return response
 
 
 @app.route('/greet')
