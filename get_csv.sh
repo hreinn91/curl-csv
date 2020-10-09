053d476907b0749a504e1c828f5c43b5349e75e8
hreinn91 <hreinnj@localhost.localdomain>
2020-10-09 18:31:42 +0200

    Added mock sftp handling

--- get_csv.sh
+++ get_csv.sh
@@ -6,3 +6,7 @@ csv_content=$(curl -sb -H "Accept: application/text" "http://localhost:5000/gene
 echo $csv_content >> response.output
 #echo $'\n' >> response.output
 cat response.output
+
+
+# Some simple sftp commands
+#sftp -i /path/to/ssh-key -b /path/to/sftp-commands.txt root@remote:/root/dropoff
